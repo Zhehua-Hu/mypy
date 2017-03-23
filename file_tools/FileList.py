ecd835e8c18c5833f0614f11242f436f1e4f54af
zhehua <zhehua@zhehua.info>
2017-03-23 22:18:58 +0800

    default update

--- file_tools/FileList.py
+++ file_tools/FileList.py
@@ -7,101 +7,138 @@ Provide FileList Class
 import os
 import argparse
 
-class FileList():
-	"""
-	class: provide file(eg.images,videos) list management
-	"""
 
-	cur_idx = 0
-	file_cnt = 0
+def get_contained_files(folder, search_type="NotRecursive",
+                        suffix_filter=None, include_hidden=False):
+    """
+        Get Contained files
+    :param folder:
+    :param search_type:
+    :param suffix_filter:
+    :param include_hidden:
+    :return:
+    """
 
-	def __init__(self, folder, search_type="NotRecursive", supported_file_suffix=None):
-		self.file_dirname = folder
-		self.file_suffix = supported_file_suffix
-		self.files_path, self.file_cnt = self.getContainedfiles(folder, type=search_type)
+    _files_path = []
+    _ret_files_path = []
+    _suffix_filter = []
+
+    # Make suffix to be ALL upper
+    if suffix_filter != None:
+        for item in suffix_filter:
+            _suffix_filter.append(item.upper())
+
+    if search_type != "Recursive":
+        for filename in os.listdir(folder):
+            file_path = os.path.join(folder, filename)
+            if os.path.isfile(file_path):
+                if len(_suffix_filter) != 0:
+                    if file_path.split(".")[-1].upper() in _suffix_filter:
+                        _files_path.append(file_path)
+                else:
+                    _files_path.append(file_path)
+    else:
+        for root, dirs, filenames in os.walk(folder):
+            for filename in filenames:
+                if not include_hidden:
+                    if not filename.startswith('.'):  # not hiden file in Linux
+                        if len(_suffix_filter) != 0:
+                            if filename.split(".")[-1].upper() in _suffix_filter:
+                                _files_path.append(os.path.join(root, filename))
+                        else:
+                            _files_path.append(os.path.join(root, filename))
+                else:
+                    if len(_suffix_filter) != 0:
+                        if filename.split(".")[-1].upper() in _suffix_filter:
+                            _files_path.append(os.path.join(root, filename))
+                    else:
+                        _files_path.append(os.path.join(root, filename))
+
+    _files_path.sort()  # sort to increase'
+
+    if include_hidden:
+        _ret_files_path = _files_path
+    else:
+        for item in _files_path:
+            satisfied = True
+            for piece in item.split(os.sep):
+                if piece.startswith('.'):
+                    satisfied = False
+            if satisfied:
+                _ret_files_path.append(item)
+
+    return _ret_files_path, len(_ret_files_path)
 
 
-	def isEmpty(self):
-		if self.file_cnt > 0:
-			return False
-		else:
-			return True
+class FileList:
+    """
+    class: provide file(eg.images,videos) list management
+    """
 
-	def getContainedfiles(self, folder, type="NotRecursive"):
-		"""
-		get Contained files
-		:param folder: searched folder
-		:param type: determine if search sub-folder
-		:return: full path list, list length
-		"""
-		files_name = []
-		files_path = []
-		ret_cnt = 0
-		for root, dirs, filenames in os.walk(folder):
-			if type == "Recursive":
-				for filename in filenames:
-					if not filename.startswith('.'): # not hiden file
-						if self.file_suffix is not None:
-							if filename.split(".")[-1].upper() in self.file_suffix:
-								files_name.append(filename)
-								ret_cnt += 1
-						else:
-							files_name.append(filename)
-							ret_cnt += 1
-			else:
-				for filename in filenames:
-					if not filename.startswith('.'): # not hiden file
-						if self.file_suffix is not None:
-							if filename.split(".")[-1].upper() in self.file_suffix:
-								files_name.append(filename)
-						else:
-							files_name.append(filename)
-				ret_cnt = len(files_name)
-				break
+    _cur_idx = 0
+    _file_cnt = 0
 
-		files_name.sort()  # from 000001 to increase
-		for item in files_name:
-			files_path.append(os.path.join(self.file_dirname, item))
-		return files_path, ret_cnt
+    def __init__(self, folder, search_type="NotRecursive",
+                 suffix_filter=None, include_hidden=False):
+        self.dirname = folder
+        self._files_path, self._file_cnt = get_contained_files(folder,
+                                                               search_type=search_type,
+                                                               suffix_filter=suffix_filter,
+                                                               include_hidden=include_hidden)
 
-	def getFilesPathList(self):
-		return self.files_path
+    def get_files_path_list(self):
+        return self._files_path
 
-	def firstFile(self):
-		return self.files_path[0]
+    def first_file(self):
+        return self._files_path[0]
 
-	def nextFile(self):
-		self.cur_idx += 1
-		self.cur_idx = self.safeLimit(self.cur_idx)
-		return self.files_path[self.cur_idx]
+    def next_file(self):
+        self._cur_idx += 1
+        self._cur_idx = self.safe_limit(self._cur_idx)
+        return self._files_path[self._cur_idx]
 
-	def previousFile(self):
-		self.cur_idx -= 1
-		self.cur_idx = self.safeLimit(self.cur_idx)
-		return self.files_path[self.cur_idx]
+    def previous_file(self):
+        self._cur_idx -= 1
+        self._cur_idx = self.safe_limit(self._cur_idx)
+        return self._files_path[self._cur_idx]
 
-	def getCurFilePath(self):
-		return self.files_path[self.cur_idx]
+    def get_cur_file_path(self):
+        return self._files_path[self._cur_idx]
 
-	def safeLimit(self, idx):
-		if idx > self.file_cnt-1:
-			return self.file_cnt-1
-		elif idx < 0:
-			return 0
-		else:
-			return idx
+    def safe_limit(self, idx):
+        if idx > self._file_cnt - 1:
+            return self._file_cnt - 1
+        elif idx < 0:
+            return 0
+        else:
+            return idx
 
-	def __repr__(self):
-		for item in self.__dict__.items():
-			print("%s : %s" % item)
+    def is_empty(self):
+        if self._file_cnt > 0:
+            return False
+        else:
+            return True
+
+    def __repr__(self):
+        for item in self.__dict__.items():
+            print("%s : %s" % item)
 
 
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser()
-	parser.add_argument("files_path_arg", help="Path of files", type=str)
-	args = parser.parse_args()
-	flist = FileList(args.files_path_arg)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("files_path_arg", help="Path of files", type=str)
+    args = parser.parse_args()
+    flist = FileList(args.files_path_arg)
 
-	# flist = FileList("/home/zhehua/Github/Enchain/libs")
-	# for item in flist.getFilesPathList():
-	# 	print(item)
+## Test
+    # test_path = "/home/zhehua/Github/mypy/test/file_tools/FileList"
+
+    # flist = FileList(test_path, search_type="Recursive")
+    # flist = FileList(test_path, search_type="Recursive", include_hidden=True)
+    # flist = FileList(test_path, search_type="Recursive", include_hidden=True, suffix_filter=["Txt"])
+    # flist = FileList(test_path, search_type="NotRecursive")
+    # flist = FileList(test_path, search_type="NotRecursive", include_hidden=True)
+    # flist = FileList(test_path, search_type="NotRecursive", include_hidden=True, suffix_filter=["Txt"])
+
+    # for item in flist.get_files_path_list():
+    #     print(item)
